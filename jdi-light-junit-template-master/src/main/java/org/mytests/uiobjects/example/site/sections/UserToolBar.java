520291d71f3c5076d31031a9babc7a11f09b7f92
Кудрявцев Александр <sasha.kudryavtsev.20@gmail.com>
2024-05-23 11:55:40 +0300

    Upgrade ToolBar

--- jdi-light-junit-template-master/src/main/java/org/mytests/uiobjects/example/site/sections/UserToolBar.java
+++ jdi-light-junit-template-master/src/main/java/org/mytests/uiobjects/example/site/sections/UserToolBar.java
@@ -3,8 +3,12 @@ package org.mytests.uiobjects.example.site.sections;
 import com.epam.jdi.light.elements.composite.Section;
 import com.epam.jdi.light.elements.pageobjects.annotations.locators.XPath;
 import com.epam.jdi.light.ui.html.elements.common.Button;
+import com.epam.jdi.light.ui.html.elements.common.Link;
 
 public class UserToolBar extends Section {
     @XPath("//a[@data-l='t,logout']")
     public Button logoutBtn;
+
+    @XPath("//a[@data-l='t,user_main']")
+    public Link user;
 }
